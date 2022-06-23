2ea9bc9ddb8fb60b662b3b2276cd73b7c2a2ae7a
Colas <riviere@umd.edu>
2022-06-23 17:30:35 +0200

    Update documentation

--- home_page.py
+++ home_page.py
@@ -7,8 +7,8 @@ st.markdown(
 '**TLDNR**: Using computer vision to detect objects and segment image.\n\n'
 'Detail:\n'
 '- Fetch an image from provided URL.\n'
-'- Object detection using DETR - DEtection TRansformer (facebook/detr-resnet-50) via huggingface and pytorch.\n'
-'- Web page generated with streamlit.\n'
+"- Computer vision using using[ Facebook's DETR - DEtection TRansformer](https://github.com/facebookresearch/detr) (detr-resnet-50 for object detection, detr_resnet101_panoptic for image segmentation) via [HuggingFace](https://huggingface.co) and [PyTorch](https://pytorch.org).\n"
+'- Web page generated with [Streamlit](https://streamlit.io).\n'
 '- **Web page hosted in a container, where???**\n'
-'- Source code: my gitlab.'
+'- Source code: [my GitLab](https://gitlab.com/colasri/detector).'
 )
