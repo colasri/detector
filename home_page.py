ee3be248f1500d3609d34ed5eebcaa9f62d0b12b
Colas <riviere@umd.edu>
2022-07-11 14:56:40 +0200

    home_page++

--- home_page.py
+++ home_page.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(layout="wide")
+st.set_page_config(layout='wide', page_title='DETR', page_icon=':eyes:')
 
 st.title('DETR playground - DEtection TRansformer')
 st.sidebar.markdown("# Home page")
@@ -10,7 +10,7 @@ st.markdown(
 'Detail:\n'
 '- Fetch an image from provided URL.\n'
 "- Computer vision using using[ Facebook's DETR - DEtection TRansformer](https://github.com/facebookresearch/detr) (detr-resnet-50 for object detection, detr_resnet101_panoptic for image segmentation) via [HuggingFace](https://huggingface.co) and [PyTorch](https://pytorch.org).\n"
-'- Web page generated with [Streamlit](https://streamlit.io).\n'
-'- Packaged with [Docker](https://www.docker.com), hosted where???**\n'
+'- Web page generated with [Streamlit](https://streamlit.io) and hosted on Streamlit Cloud.\n'
+'- Optionally packaged with [Docker](https://www.docker.com), self-hosted.\n'
 '- Source code: [my GitLab](https://gitlab.com/colasri/detector).'
 )
