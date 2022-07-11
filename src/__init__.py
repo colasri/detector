3b840a826797ab0c6e15968912dc9f6c37279ab0
Colas <riviere@umd.edu>
2022-07-11 13:17:48 +0200

    Adaptable threshold commit leftover

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,7 @@
-from .detect import detector
+from .detect import detector, draw_detection
 from .segment import segmentor
 
 # default_url = 'https://i.redd.it/9eh6phbzw2t31.jpg'
-default_url = 'https://img.lemde.fr/2022/05/30/801/0/6016/3004/1440/720/75/0/e7cbb12_1653920831641-pictorium-lepictorium-0185808.jpg'
+# default_url = 'https://img.lemde.fr/2022/05/30/801/0/6016/3004/1440/720/75/0/e7cbb12_1653920831641-pictorium-lepictorium-0185808.jpg'
+# default_url = 'https://www.grenoblealpesmetropole.fr/uploads/Actualite/1d/IMF_100/GAB_LA_METRO/1582_536_VISITE-CHANTIER-RONDEAU-LAME-TRO-THIBAULT-LEFEBURE-2022-31.jpg'
+default_url = 'https://www.convergence-velo-grenoble.fr/media/FAHn2YwX0AEqxLZ.jpg'
